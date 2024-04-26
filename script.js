44283e09cb8130be1c623550f4d5bf24f4f8bbc5
Saikumaradepelli07 <122333649+Saikumaradepelli07@users.noreply.github.com>
2024-04-26 19:43:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const str = [["avfbfbngotbongbiguhbosfbhr"], ["gdkygsodhcg"]];
+console.log(str.length);
+const str2 = "abcdefghijklmnopqrstuvwxyz";
+console.log(str2.length);
+const upe = "abcdefgh";
+console.log(upe.toUpperCase());
+const low = "ABCDFT";
+console.log(low.toLocaleLowerCase());
